f6bbe9d6fb73a3ad9c1989056d0027d2f0c381cf
Mateo Franco <mfranco@uc.edu.co>
2024-01-09 20:51:14 -0500

    web site

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("excel");
+        habilidades[1].classList.add("power_bi");
+        habilidades[2].classList.add("tableau");
+        habilidades[3].classList.add("mysql_postgresql");
+        habilidades[4].classList.add("python");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
