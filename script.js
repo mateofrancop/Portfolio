c3d23957937d2be97ab8925d77806e4745300011
Mateo Franco <mfranco@uc.edu.co>
2024-01-10 15:04:16 -0500

    web site

--- script.js
+++ script.js
@@ -32,10 +32,40 @@ function efectoHabilidades(){
         habilidades[8].classList.add("dedicacion");
         habilidades[9].classList.add("proyect");
     }
-}
-
+};
 
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
 } 
+
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
+
+    // Recoger los datos del formulario
+    const form = event.target;
+    const formData = new FormData(form);
+
+    // Enviar el formulario usando Fetch
+    fetch(form.action, {
+        method: form.method,
+        headers: {
+            'Accept': 'application/json'
+        },
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        // Mostrar mensaje de confirmación al usuario
+        alert('Message sent succesfully!');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred while sending the message. Please try again.');
+    });
+});
+
+
+
